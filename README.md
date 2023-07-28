6e97f0ff91cb2eb387cef5c22e4d0dda3cb82d25
Jean Carlo <jeansanto2002@gmail.com>
2023-07-28 16:43:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aplicação TypeScript
+
+Aplicação de negociação de bolsa de valores, utilizando o TypeScript.
+
+:construction: Projeto em construção :construction:
---
+++ app/models/negociacoes.ts
@@ -0,0 +1,12 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+    private negociacoes: Negociacao[] = []; // Array<Negociacao>
+
+    adiciona(negociacao: Negociacao){
+        this.negociacoes.push(negociacao);
+    }
+    lista(): readonly Negociacao[]{ // ReadonlyArray<Negociacao>
+        return this.negociacoes;
+    }
+}
