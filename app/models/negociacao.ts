8f3a2efce795fa9d2cb0fb133c5003d9eff6c25a
Jean Carlo <jeansanto2002@gmail.com>
2023-07-29 14:29:15 -0300

    modulo: Avançando na linguagem

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -10,15 +10,15 @@ export class Negociacao {
         return data;
     }
 
-    // get quantidade(): number {
-    //     return this._quantidade;
-    // }
-
-    // get valor(): number {
-    //     return this._valor;
-    // }
-
     get volume(): number {
         return this.quantidade * this.valor;
+    }
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
     }
 }
--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,10 +3,10 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
     private negociacoes: Negociacao[] = []; // Array<Negociacao>
 
-    adiciona(negociacao: Negociacao){
+    public adiciona(negociacao: Negociacao){
         this.negociacoes.push(negociacao);
     }
-    lista(): readonly Negociacao[]{ // ReadonlyArray<Negociacao>
+    public lista(): readonly Negociacao[]{ // ReadonlyArray<Negociacao>
         return this.negociacoes;
     }
 }
